1c3abdfc34bb6f58f1d9c6b66e7180546e9435f3
MRbcgd <quittypig@gmail.com>
2018-09-13 13:24:54 +0900

    change main js name run_web.js -> server.js

--- server.js
+++ server.js
@@ -1,115 +1,101 @@
 /*
     author     : bak chulhyong
     created    : 2018 - 09 - 12
-    modified   : 2018 - 09 - 13
-    description: server
+    modified   : 2018 - 09 - 12
+    description: main
 */
-const fs   = require('fs');
-const http = require('http');
-const url  = require('url');
+const http    = require('http');
+const cluster = require('cluster');
 
-const LOG    = require('./common/log.js');
 const EQUIP  = require('./common/equip.js');
-const ROUTER = require('./router/module.js');
+const LOG    = require('./common/log.js');
 
-// HTTP
-const PORT = EQUIP.getConfig('port') || 8080;
+// LOG
+const LOG_PATH = EQUIP.getLogDir();
+LOG.setLogPath(LOG_PATH);
 
-var g_server = null; // SERVER
+// SERVER
+const SERVER = require('./socket_server.js');
 
-// module
-function getModule ( arr_path ) {
-    var page      = arr_path.shift() || 'default';
-    var page_mode = arr_path.shift() || 'default';
-    var func_mode = arr_path.shift() || 'default';
+var g_is_shutdown   = false; // grcaefulshutdown
 
-    var pg_list = ROUTER['PG_LIST'][page]       || ROUTER['PG_LIST']['default'];
-    var md_list = pg_list['MD_LIST'][page_mode] || pg_list['MD_LIST']['default'];
-    var fc_list = md_list['FC_LIST'][func_mode] || md_list['FC_LIST']['default'];
+function gracefulShutdown () {
+    LOG.log(null, '###############################################');
+	LOG.log(null, "[gracefulShutdown READY]");
+    LOG.log(null, '###############################################');
 
-    return fc_list;
+	if( g_is_shutdown == true ) return;
+	g_is_shutdown = true;
+
+	SERVER.stopServer();
+
+	setTimeout(function() {
+        LOG.log(null, '###############################################');
+        LOG.log(null, "[gracefulShutdown DONE]");
+        LOG.log(null, '###############################################');
+
+		process.exit();
+	}, 30*1000); // forcefully shut down 30s later
 }
 
-function onServerRecvAfter( req, res )
-{
-    LOG.log(null, "RECEIVED URL: " + req.url);
-
-    var url_qry  = EQUIP.parseURL(req.url);
-	var url_obj  = null;
-
-    if ( url_qry.arr_path == null ) url_qry.arr_path = [];
-    getModule(url_qry.arr_path)(req, res);
-    return;
-}
-function onServerAccepted(req, res)
-{
-	LOG.log(null, "####################################################################");
-	LOG.log(null, "[CLIENT] " + EQUIP.getRequestPublicIP(req));
-    LOG.log(null, "####################################################################");
-
-	if( req.method == 'POST' ) {
-		LOG.log(null, "[METHOD]: POST");
-
-		var body = '';
-		// Chainning
-		req.on('data', function (data) {
-			body += data;
-			if (body.length > 1e6) {
-				req.connection.destroy();
-			}
-		});
-		req.on('end', function () {
-			req.body = body;
-			onServerRecvAfter(req, res);
-		});
-	} else {
-		LOG.log(null, "[METHOD]: GET");
-		onServerRecvAfter(req, res);
-	}
-}
-
-module.exports.runServer = function() {
-    g_server = http.createServer();
-
-	g_server.on('close', function( err, socket ) {
-		LOG.log(null, "[SERVER] onClose");
+function setProcess ( child ) {
+	child.on('warning', function(warning) {
+		console.warn("[CHILD] " + warning.name);    // Print the warning name
+		console.warn("[CHILD] " + warning.message); // Print the warning message
+		console.warn("[CHILD] " + warning.stack);   // Print the stack LOG
 	});
-
-	g_server.on('connection', function( socket ) {
-		var addr_ip   = socket.remoteAddress;
-		var addr_port = socket.remotePort;
-		LOG.log(null, "[SERVER] onAccepted [" + addr_ip + ":" + addr_port + "]");
-
-		socket.addr_ip   = addr_ip;
-		socket.addr_port = addr_port;
-
-		socket.on('close', function( socket ) {
-			LOG.log(null, "[SOCKET] onClosed [" + addr_ip + ":" + addr_port + "]");
-		});
+	child.on('unhandledRejection', function(reason, p) {
+		LOG.err(null, "[CHILD] Unhandled Rejection at: Promise", p, ' reason:', reason);
+		console.error('[unhandledRejection] Promise', p, ' reason:', reason);
+		child.exit(-2);
 	});
-
-	g_server.on('request', onServerAccepted);
-
-	g_server.on('clientError', (err, socket) => {
-		socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-
-		var addr_ip   = socket.remoteAddress;
-		var addr_port = socket.remotePort;
-
-		LOG.err(null, "[SERVER] CLIENT ERROR [" + addr_ip + ":" + addr_port + "]");
+	child.on('uncaughtException', function(err) {
+		LOG.err(null, "[CHILD] Caught exception: ", err, ' stack:', err.stack);
+		console.error('[uncaughtException]', err);
+		child.exit(-1);
 	});
-
-	g_server.listen(PORT);
-
-    console.log("SERVER RUNNING AT PORT: " + PORT);
-	LOG.log(null, "SERVER RUNNING AT PORT: " + PORT);
+	child.on('SIGTERM', function() {
+		LOG.log(null, "[CHILD] Caught SIGTERM");
+		console.log('[SIGTERM]');
+		gracefulShutdown();
+	});
+	child.on('SIGINT', function() {
+		LOG.log(null, "[CHILD] Caught SIGINT");
+		console.log('[SIGTERM]');
+		gracefulShutdown();
+	});
+	child.on('exit', function(code) {
+		LOG.log(null, "[CHILD] About to exit with code:[" + code + "]");
+	});
 }
 
-module.exports.stopServer = function() {
-    if( g_server != null ) {
-        g_server.close(function() {
-            LOG.log(null, "SERVER STOPED, CLOSED CONNECTIONS");
-            process.exit();
-        });
+// CLUSTERING
+if( cluster.isMaster ) {
+	LOG.log(null, '###############################################');
+	LOG.log(null, '# RUNNING [PID:' + process.pid + ']');
+	LOG.log(null, '###############################################');
+
+	// Fork workers.
+    var numCPUs = require('os').cpus().length;
+    for( var i = 0; i < numCPUs; i++ ) {
+        cluster.fork();
     }
+
+    cluster.on('online', function(worker) {
+		LOG.log(null, '# WORKER [PID:' + worker.process.pid + '] ONLINE');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+		LOG.err(null, '# WORKER [PID:' + worker.process.pid + '] DIED[' + code + ',' + signal + ']');
+		cluster.fork();
+    });
+} else {
+	setProcess(process);
+
+    // CHILD
+	LOG.log(null, '-----------------------------------------------');
+	LOG.log(null, '- Running [PID:' + process.pid + ']');
+	LOG.log(null, '-----------------------------------------------');
+
+    SERVER.runServer();
 }
---
+++ socket_server.js
@@ -0,0 +1,114 @@
+/*
+    author     : bak chulhyong
+    created    : 2018 - 09 - 12
+    modified   : 2018 - 09 - 13
+    description: server
+*/
+const fs   = require('fs');
+const http = require('http');
+const url  = require('url');
+
+const LOG    = require('./common/log.js');
+const EQUIP  = require('./common/equip.js');
+const ROUTER = require('./router/module.js');
+
+// HTTP
+const PORT = EQUIP.getConfig('port') || 8080;
+
+var g_server = null; // SERVER
+// module
+function getModule ( arr_path ) {
+    var page      = arr_path.shift() || 'default';
+    var page_mode = arr_path.shift() || 'default';
+    var func_mode = arr_path.shift() || 'default';
+
+    var pg_list = ROUTER['PG_LIST'][page]       || ROUTER['PG_LIST']['default'];
+    var md_list = pg_list['MD_LIST'][page_mode] || pg_list['MD_LIST']['default'];
+    var fc_list = md_list['FC_LIST'][func_mode] || md_list['FC_LIST']['default'];
+
+    return fc_list;
+}
+
+function onServerRecvAfter( req, res )
+{
+    LOG.log(null, "RECEIVED URL: " + req.url);
+
+    var url_qry  = EQUIP.parseURL(req.url);
+	var url_obj  = null;
+
+    if ( url_qry.arr_path == null ) url_qry.arr_path = [];
+    getModule(url_qry.arr_path)(req, res);
+    return;
+}
+function onServerAccepted(req, res)
+{
+	LOG.log(null, "####################################################################");
+	LOG.log(null, "[CLIENT] " + EQUIP.getRequestPublicIP(req));
+    LOG.log(null, "####################################################################");
+
+	if( req.method == 'POST' ) {
+		LOG.log(null, "[METHOD]: POST");
+
+		var body = '';
+		// Chainning
+		req.on('data', function (data) {
+			body += data;
+			if (body.length > 1e6) {
+				req.connection.destroy();
+			}
+		});
+		req.on('end', function () {
+			req.body = body;
+			onServerRecvAfter(req, res);
+		});
+	} else {
+		LOG.log(null, "[METHOD]: GET");
+		onServerRecvAfter(req, res);
+	}
+}
+
+module.exports.runServer = function() {
+    g_server = http.createServer();
+
+	g_server.on('close', function( err, socket ) {
+		LOG.log(null, "[SERVER] onClose");
+	});
+
+	g_server.on('connection', function( socket ) {
+		var addr_ip   = socket.remoteAddress;
+		var addr_port = socket.remotePort;
+		LOG.log(null, "[SERVER] onAccepted [" + addr_ip + ":" + addr_port + "]");
+
+		socket.addr_ip   = addr_ip;
+		socket.addr_port = addr_port;
+
+		socket.on('close', function( socket ) {
+			LOG.log(null, "[SOCKET] onClosed [" + addr_ip + ":" + addr_port + "]");
+		});
+	});
+
+	g_server.on('request', onServerAccepted);
+
+	g_server.on('clientError', (err, socket) => {
+		socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+
+		var addr_ip   = socket.remoteAddress;
+		var addr_port = socket.remotePort;
+
+		LOG.err(null, "[SERVER] CLIENT ERROR [" + addr_ip + ":" + addr_port + "]");
+	});
+
+	g_server.listen(PORT);
+
+    console.log("SERVER RUNNING AT PORT: " + PORT);
+	LOG.log(null, "SERVER RUNNING AT PORT: " + PORT);
+}
+
+module.exports.stopServer = function() {
+    if( g_server != null ) {
+        g_server.close(function() {
+            LOG.log(null, "SERVER STOPED, CLOSED CONNECTIONS");
+            process.exit();
+        });
+    }
+}
