4c5ad65bbd32e2c962b73a046d431408360c74ba
MRbcgd <quittypig@gmail.com>
2018-09-22 22:43:08 +0900

    change staging pwd

--- common/config.js
+++ common/config.js
@@ -13,7 +13,7 @@ module.exports.config = {
         "host"              : 'us-cdbr-iron-east-01.cleardb.net',
         "port"              : 3306                              ,
         "user"              : 'bb199af2d42a06'                  ,
-        "password"          : 'c2495206213a2fe'                 ,
+        "password"          : '98b5d36aba5b5ae'                 ,
         "database"          : 'heroku_8dc2d6c2cd1cfdb'          ,
         "connectionLimit"   : 0                                 ,
         "waitForConnections": true                              ,
